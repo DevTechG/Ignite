d126787d4e2be5a38656cf97aaf048a82b6f015b
DevTechG <gabriel10300007@gmail.com>
2025-02-13 16:15:59 -0300

    Criando componente de Post.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-function App() {
+// JSX = JavaScript + XML
+
+import { Post }from "./Post"
+
+export function App() {
 
   return (
-    <h1> Hello World </h1>
+    <div>
+      <Post />
+      <Post />
+    </div>
   )
 }
 
---
+++ src/Post.jsx
@@ -0,0 +1,12 @@
+/* // Default Exports
+function Post() {
+    return <p>Post</p>
+}
+export default Post
+ */
+
+// Named Exports
+export function Post() {
+    return <p>Post</p>
+}
+// Default Exports vs Named Exports
