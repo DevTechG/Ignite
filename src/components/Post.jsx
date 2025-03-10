a6b5b3ab6a926168ff5fdc6939b7e14380f069c7
DevTechG <gabriel10300007@gmail.com>
2025-03-10 15:56:53 -0300

    Creating a Post comment form.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,6 +4,7 @@ export function Post() {
   return (
     <article className={styles.post}>
       <header>
+
         <div className={styles.author}>
           <img className={styles.avatar} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
         />
@@ -26,6 +27,18 @@ export function Post() {
           <a href="">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
