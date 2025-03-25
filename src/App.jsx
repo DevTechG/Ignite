05aea8ea86289632eecb25f76556e1bbe2d2c19f
DevTechG <gabriel10300007@gmail.com>
2025-03-25 18:52:13 -0300

    Creating interactive posts in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,37 @@ import styles from './App.module.css';
 import './global.css'
 
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/DevTechG.png',
+      name: 'DevTechG',
+      role: 'Full-Stack Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portfólio.' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2025-03-20 11:30:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50',
+      name: 'DeveloperRandom',
+      role: 'Front-end'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Fiz um novo projeto.' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
+
 export function App() {
 
   return (
@@ -18,14 +49,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author = "Author01" 
-            content = "Lorem ipsum dolor sit amet consectetur  dipisicing elit. Deserunt, numquam."
-          />
-          <Post 
-            author = "Author02" 
-            content = "Lorem."
-          />
+          {posts.map(post => {
+              return (
+                <Post
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            })}
         </main>
       </div>
     </div>
