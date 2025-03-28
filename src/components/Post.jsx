1595302aa67808a294abc053b753ea6e41b8a66e
DevTechG <gabriel10300007@gmail.com>
2025-03-28 10:17:15 -0300

    Adding useState to dynamically render user comments.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,8 +4,13 @@ import styles from './Header.module.css'
 
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    1,
+    2,
+  ]);
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
@@ -15,6 +20,12 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
     addSuffix: true
   });
+
+  function handleCrateNewComment() {
+    event.preventDefault()
+
+    setComments([...comments, comments.length + 1]);
+  }
 
   return (
     <article className={styles.post}>
@@ -42,7 +53,7 @@ export function Post({ author, publishedAt, content }) {
         }})}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea
@@ -55,10 +66,10 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
-        
+        {comments.map(comment => {
+          return <Comment />
+          })}
+         
       </div>
       
     </article>
