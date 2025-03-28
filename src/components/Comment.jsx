975db574c0ecaa6d191da42eeea16f64b5088932
DevTechG <gabriel10300007@gmail.com>
2025-03-28 13:23:41 -0300

    Creating a delete comment function.

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,12 @@ import styles from './Comment.module.css';
 
 import { ThumbsUp, Trash } from 'phosphor-react';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
   return (
     <div className={styles.comment}>
      <Avatar hasBorder={false}  src="https://images.unsplash.com/photo-1741412252402-0d965ea9970b?auto=format&fit=crop&w=500&q=50" alt="" />
@@ -16,7 +21,7 @@ export function Comment({ content }) {
               <time title="10 de Março às 20:00h" dateTime="2025-03-10 20:00:00">Cerca de 1h atrás</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -38,6 +38,10 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value);
   }
 
+  function deleteComment(comment) {
+    console.log(`Deletar comentário ${comment}`)
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -58,9 +62,9 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
       {content.map(line => {
         if (line.type === 'paragraph') {
-          return <p>{line.content}</p>;
+          return <p key={line.content}>{line.content}</p>;
         } else if (line.type === 'link') {
-         return <p><a href="#">{line.content}</a></p>
+         return <p key={line.content}><a href="#">{line.content}</a></p>
         }})}
       </div>
 
@@ -81,7 +85,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment content={comment} />
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          )
           })}
          
       </div>
